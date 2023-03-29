3fa36353c1b380ecf36fbd09860b4631f48cb8a8
Damgom <wisewish1@gmail.com>
2023-03-29 18:53:48 +0900

    INIT: email send pratice

---
+++ email-send-practice/src/main/java/com/damgom/emailsendpractice/EmailSendPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.damgom.emailsendpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSendPracticeApplication.class, args);
+	}
+
+}
---
+++ email-send-practice/src/test/java/com/damgom/emailsendpractice/EmailSendPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damgom.emailsendpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
