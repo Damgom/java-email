5c405e74a4f2ece28da13b119437a3d574fea727
Damgom <wisewish1@gmail.com>
2023-03-29 19:14:40 +0900

    FEAT: simple mail send service logic

---
+++ email-send-practice/src/main/java/com/damgom/emailsendpractice/service/MailService.java
@@ -0,0 +1,23 @@
+package com.damgom.emailsendpractice.service;
+
+import com.damgom.emailsendpractice.dto.MailDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    private JavaMailSender mailSender;
+    @Value("${spring.mail.username")
+    private String FROM_ADDRESS;
+
+    public void mailSend(MailDto mailDto) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mailDto.getAddress());
+        mailMessage.setFrom(FROM_ADDRESS);
+        mailMessage.setSubject(mailDto.getTitle());
+        mailMessage.setText(mailDto.getMessage());
+        mailSender.send(mailMessage);
+    }
+}
